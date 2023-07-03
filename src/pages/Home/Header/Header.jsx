b766e7f1784b6b877ccb90733b2d91970c672f45
moktubat <shadhinreja@gmail.com>
2023-07-03 18:30:48 +0600

    typed js added

--- src/pages/Home/Header/Header.jsx
+++ src/pages/Home/Header/Header.jsx
@@ -1,24 +1,40 @@
 import { Player } from "@lottiefiles/react-lottie-player";
-import hero from "../../../assets/hero.png";
-import handshake from "../../../assets/handshake.json";
+import coding from "../../../assets/coding.json";
+import team from "../../../assets/team.png";
+import time from "../../../assets/time.png";
+import learner from "../../../assets/learner.png";
+import communication from "../../../assets/communication.png";
+import Typed from 'typed.js';
+import { useEffect, useRef } from "react";
+
 const Header = () => {
+    const el = useRef(null);
+
+    useEffect(() => {
+        const typed = new Typed(el.current, {
+            strings:["Web Developer", "MERN Stack Developer"],
+            startDelay:300,
+            typeSpeed:150,
+            backDelay:150,
+            smartBackspace:true,
+            showCursor:false,
+            loop:true,
+        })
+    })
   return (
     <div className="container">
       <div className="relative w-full h-full flex flex-col justify-center items-center bg-gray-50 overflow-hidden">
         <div className="pt-6 sm:pt-12 lg:pt-24 w-full flex flex-col lg:flex-row">
           <div className="w-full lg:w-1/2 flex flex-col justify-center items-center text-gray-600">
             <div className="p-5 flex flex-col justify-center items-center lg:items-start text-center lg:text-left">
-              <h1 className="py-10 text-3xl sm:text-5xl font-bold tracking-wide text-gray-900">
-                Discover the fascinating
-                <br />
-                LEGO manifested in the
-                <br /> ultimate LEGO building
+              <h1 className="py-5 text-3xl sm:text-5xl font-bold tracking-wide text-gray-900">
+              Hi, I am Moktubat Jaman                
               </h1>
+              <h3 className="text-1xl sm:text-5xl text-gray-900 pb-2">
+              I am <span ref={el}></span>
+              </h3>
               <p className="text-lg font-semibold text-gray-400 tracking-wide">
-                Get your favourite LEGO® toys,
-                <br />
-                LEGOVILLA® Exclusive merchandise and
-                <br /> much more at our LEGOVILLA® Retail Store!
+              I am a MERN Stack Front-end developer. I have experience building RESTful APIs, working with MongoDB for data storage and retrieval, building strong backend systems using Node.js and Express.js, and designing clear user interfaces with React.
               </p>
 
               <div className="mt-10 flex flex-col sm:flex-row items-center">
@@ -52,97 +68,48 @@ const Header = () => {
           </div>
 
           <div className="relative w-full lg:w-1/2 flex flex-col justify-center items-center overflow-hidden">
-            <img src={hero} alt="" className="w-2/3 lg:w-full rounded" />
+            <Player
+              autoplay
+              loop
+              src={coding}
+              style={{ height: "300px", width: "300px" }}
+              className="w-2/3 lg:w-full rounded"
+            ></Player>
           </div>
         </div>
 
         {/* :FEATURES */}
         <div className="container mt-14 p-5 w-full grid grid-cols-12 sm:grid-cols-5 gap-2 text-xs md:text-sm">
           <div className="col-span-4 sm:col-span-1 inline-flex flex-col items-center border-r border-gray-300 text-gray-400 uppercase">
-            <Player
-              autoplay
-              loop
-              src={handshake}
-              style={{ height: "86px", width: "86px" }}
-            >
-            </Player>
+            <img
+              className="w-[40px] h-[40px] mb-2"
+              src={communication}
+              alt=""
+            />
             <span className="text-center">A Positive Attitude</span>
           </div>
 
-          {/* ::Four Sizes */}
           <div className="col-span-4 sm:col-span-1 inline-flex flex-col items-center border-r border-gray-300 text-gray-400 uppercase">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-9 w-9"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01"
-              />
-            </svg>
+            <img
+              className="w-[40px] h-[40px] mb-2"
+              src={communication}
+              alt=""
+            />
             <span className="text-center">Good Communication Skills</span>
           </div>
 
-          {/* ::Quality Materials */}
           <div className="col-span-4 sm:col-span-1 inline-flex flex-col items-center border-r border-gray-300 text-gray-400 uppercase">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-9 w-9"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M5 13l4 4L19 7"
-              />
-            </svg>
+            <img className="w-[40px] h-[40px] mb-2" src={time} alt="" />
             <span className="text-center">Good Time and Task Management</span>
           </div>
 
-          {/* ::Fast Shipping */}
           <div className="col-span-6 sm:col-span-1 inline-flex flex-col items-center border-r border-gray-300 text-gray-400 uppercase">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-9 w-9"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path d="M9 17a2 2 0 11-4 0 2 2 0 014 0zM19 17a2 2 0 11-4 0 2 2 0 014 0z" />
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M13 16V6a1 1 0 00-1-1H4a1 1 0 00-1 1v10a1 1 0 001 1h1m8-1a1 1 0 01-1 1H9m4-1V8a1 1 0 011-1h2.586a1 1 0 01.707.293l3.414 3.414a1 1 0 01.293.707V16a1 1 0 01-1 1h-1m-6-1a1 1 0 001 1h1M5 17a2 2 0 104 0m-4 0a2 2 0 114 0m6 0a2 2 0 104 0m-4 0a2 2 0 114 0"
-              />
-            </svg>
+            <img className="w-[40px] h-[40px] mb-2" src={learner} alt="" />
             <span className="text-center">Quick Learning Ability</span>
           </div>
 
-          {/* ::Layflat Page */}
           <div className="col-span-6 sm:col-span-1 inline-flex flex-col items-center border-r-none border-gray-300 text-gray-400 uppercase">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-9 w-9"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"
-              />
-            </svg>
+            <img className="w-[40px] h-[40px] mb-2" src={team} alt="" />
             <span className="text-center">A Good Team Player</span>
           </div>
         </div>
